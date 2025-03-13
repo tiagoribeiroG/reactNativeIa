ee5678dd432e785213dda81c35a92bd71b3c51dc
tiagoribeiroG <tiagoribeiro1246@gmail.com>
2025-03-13 09:49:30 -0300

    featGpt

--- app/index.tsx
+++ app/index.tsx
@@ -9,21 +9,68 @@ import {
   Pressable,
   ScrollView,
   ActivityIndicator,
+  Alert,
+  Keyboard,
 } from "react-native";
 import React, { useState } from "react";
 import { MaterialIcons } from "@expo/vector-icons";
 import Slider from "@react-native-community/slider";
+import { API_KEY } from '@env';
 
 const statusBarHeight = StatusBar.currentHeight;
+console.log(API_KEY);
+
 
 export default function App() {
   const [city, setCity] = useState("");
-  const [days, setDays] = useState(1);
+  const [days, setDays] = useState(1);5
   const [loading, setLoading] = useState(false);
   const [travel, setTravel] = useState("");
 
-  function handleGenerate() {
+  async function handleGenerate() {
+    if(city === ""){
+      Alert.alert("Atençao", "Preencha o nome da Cidade")
+      return;
+
+    }
     setLoading(true);
+    Keyboard.dismiss();
+    const prompt = `Crie um roteiro para viagem de exatos ${days} dias na cidade ${city}, busque lugares turisticos, lugares mais visitados, forneca apenas em topicos com nome de local e onde ir em cada dia`
+
+
+    fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers:{
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${API_KEY}`
+      },
+      body: JSON.stringify({
+        model: "gpt-4o-mini",
+        message:[
+          {
+            role: 'user',
+            content: prompt
+          }
+        ], 
+        temperature: 0.20,
+        max_tokens: 500,
+        top_p: 1,
+      })
+
+    })
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data.choices[0].message.content);
+      setTravel(data.choices[0].message.content)
+
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+    .finally(() => {
+      setLoading(false);
+    })
+
     setTimeout(() => {
       setTravel(`Roteiro para ${city} por ${days.toFixed(0)} dias.`);
       setLoading(false);
